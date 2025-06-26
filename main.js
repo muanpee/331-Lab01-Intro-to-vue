76a063a68ebb800f0c576e66a250c28ba996d3f7
muanpee <muanpee_t@cmu.ac.th>
2025-06-27 00:14:07 +0700

    add on sale

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const { createApp, ref } = Vue;
 
 createApp({
-  setup() {
-    const product = ref('Boots');
-    const image = ref('./assets/images/socks_green.jpg');
-    const link = ref('https://www.camt.cmu.ac.th');
-
-    return {
-      product,image, link
+    setup() {
+        const product = ref('Socks')
+        const image = ref('./assets/images/socks_green.jpg')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onsale = ref(true)
+        return {
+            product,
+            image,
+            inStock,
+            inventory,
+            onsale
+        }
     }
-    }
 
-}).mount('#app');
+}).mount('#app')
