4d90998719dfa4dd86636fec987dcb417d4546df
muanpee <muanpee_t@cmu.ac.th>
2025-06-27 09:05:44 +0700

    add size the sock s,m,l

--- main.js
+++ main.js
@@ -7,12 +7,18 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(100)
         const onsale = ref(true)
+        const details = ref(['50% cotton', '30% wool', '20% polyester'])
+        const variants = ref([ 
+            { id: 2234, color: 'green'},
+            { id: 2235, color: 'blue'},
+        ])
+        const sizes = ref(['S ', 'M ', 'L '])
         return {
             product,
             image,
             inStock,
             inventory,
-            onsale
+            onsale,details,variants
         }
     }
 
