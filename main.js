1c0af54cd5e2a649a0e5cc89c7e86ee9502e50e6
muanpee <muanpee_t@cmu.ac.th>
2025-06-27 10:12:23 +0700

    add brand product 8.5

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const { createApp, ref } = Vue;
+const { createApp, ref ,computed} = Vue;
 
 createApp({
     setup() {
         const product = ref('Socks')
-        const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(true)
+        const brand = ref('SE 331')
+
         const inventory = ref(100)
         const onsale = ref(true)
         const details = ref(['50% cotton', '30% wool', '20% polyester'])
         const variants = ref([ 
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0},
         ])
+        const selectedVariant = ref(0)
         const sizes = ref(['S ', 'M ', 'L '])
         const cart = ref(0)
+
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
 
         function addToCart() {
             cart.value += 1
@@ -26,13 +31,24 @@ createApp({
             inventory.value = inventory.value > 0 ? 0 :100;
         }
 
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
+
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image;
+        })
+
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        })
 
         return {
             product,
             image,
             inStock,
             inventory,
-            onsale,details,variants,sizes,addToCart,updateImage,cart,stockStatus
+            onsale,details,variants,sizes,addToCart,updateImage,cart,stockStatus,title,brand,updateVariant
         }
     }
 
